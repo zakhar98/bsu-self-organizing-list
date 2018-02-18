3f7cec4e2b796051242f4889edbc26700e0bea78
spartanez17 <spartanez17@gmail.com>
2018-02-18 22:49:54 +0300

    finished but not tested

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -8,17 +8,17 @@ class Node {
 
 class SelfOrganizedList {
 
-    size = 0;
-
     constructor() {
         this.head = null;
         this.tail = null;
+        this.count = 0;
     }
 
     insert(data) {
-        if (this.head === null) {
+        if (!this.head) {
             this.head = new Node(data);
             this.tail = new Node(data);
+            return;
         }
         let newElem = new Node(data);
         newElem.next = this.head;
@@ -27,19 +27,19 @@ class SelfOrganizedList {
     }
 
     size() {
-        return this.size;
+        return this.count;
     }
 
     at(index) {
         if (index < 0 || index >= size) {
-            return false;
+            return null;
         }
         for (let p = this.head; p.next !== null; p = p.next) {
-            if (index-- == 0) {
+            if (index-- === 0) {
                 return p.data;
             }
         }
-        return false;
+        return null;
     }
 
     findNode(data) {
@@ -53,27 +53,69 @@ class SelfOrganizedList {
     }
 
     toArray() {
-
+        let arr = [this.count];
+        let i = 0;
+        for (let p = this.head; p.next !== null; p = p.next) {
+            arr[i] = p.data;
+        }
+        return arr;
     }
 
     removeAt(index) {
-        if (index >= 0 || index < size) {
-
-            for (let p = this.head; p.next !== null; p = p.next) {
-                
-                if (index-- == 0) {
-                    return p.data;
-                }
-            }
+        if (index < 0 || index >= size) {
+            return null;
         }
+        del = this.findNode(at(index));
+        if (del === null) {
+            return;
+        }
+        this.count--;
+        if (this.head === del) {
+            this.head = del.next;
+            this.head.prev = null;
+            del.next = null;
+            return;
+        }
+        if (this.tail === del) {
+            this.tail = del.prev;
+            this.tail.next = null;
+            del.prev = null;
+            return;
+        }
+        let a = del.prev;
+        let b = del.next;
+        a.next = b;
+        b.prev = a;
+        del.prev = del.next = null;
     }
 
     moveToFront(node) {
-
+        if (this.head === node) {
+            return;
+        }
+        else if (this.tail === node) {
+            this.tail = node.prev;
+            this.tail.next = null;
+            node.prev = null;
+            node.next = this.head;
+            this.head = null;
+            return;
+        } else {
+            let a = node.prev;
+            let b = node.next;
+            a.next = b;
+            b.prev = a;
+            node.prev = null;
+            node.next = this.head;
+        }
     }
 
     reorganize(data) {
-
+        popular = this.findNode(data);
+        if(!curr){
+            return;
+        }
+        this.moveToFront(popular);
     }
 
 }
