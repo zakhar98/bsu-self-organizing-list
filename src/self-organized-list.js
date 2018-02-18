3c02d65806e3fdb2ed2b4cac462cb70ba67341b9
spartanez17 <spartanez17@gmail.com>
2018-02-18 17:52:20 +0300

    TODO

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -7,25 +7,49 @@ class Node {
 }
 
 class SelfOrganizedList {
+
+    size = 0;
+
     constructor() {
         this.head = null;
         this.tail = null;
     }
 
     insert(data) {
-
+        if (this.head === null) {
+            this.head = new Node(data);
+            this.tail = new Node(data);
+        }
+        let newElem = new Node(data);
+        newElem.next = this.head;
+        this.head.prev = newElem;
+        this.head = newElem;
     }
 
     size() {
-
+        return this.size;
     }
 
     at(index) {
-
+        if (index < 0 || index >= size) {
+            return false;
+        }
+        for (let p = this.head; p.next !== null; p = p.next) {
+            if (index-- == 0) {
+                return p.data;
+            }
+        }
+        return false;
     }
 
     findNode(data) {
 
+        for (let p = this.head; p.next !== null; p = p.next) {
+            if (p.data = data) {
+                return p;
+            }
+        }
+        return false;
     }
 
     toArray() {
@@ -33,7 +57,15 @@ class SelfOrganizedList {
     }
 
     removeAt(index) {
+        if (index >= 0 || index < size) {
 
+            for (let p = this.head; p.next !== null; p = p.next) {
+                
+                if (index-- == 0) {
+                    return p.data;
+                }
+            }
+        }
     }
 
     moveToFront(node) {
